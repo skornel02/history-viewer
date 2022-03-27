1d8ab2293111becedc61b74d00f7ea53314caab8
Kornél <stefankornel02@gmail.com>
2022-03-27 20:38:48 +0200

    Better readibility

--- src/CustomReaderCss.tsx
+++ src/CustomReaderCss.tsx
@@ -5,13 +5,29 @@ const css = `
     padding: 10px;
     margin: 0 auto;
 }
+#contentBody>div {
+    padding: 10px;
+    margin: 0 !important;
+}
 `;
+
+const lefter: string[] = [];
+for (let i = 0; i <= 20; i++) {  
+    lefter.push(
+`.c${i} {
+    margin-left: ${i * 2}vw !important;
+}`
+);
+}
 
 const CustomReaderCss: React.FunctionComponent = () => {
     return (
         <div id="ReaderCss">
             <style>
                 {css}
+            </style>
+            <style>
+                {lefter}
             </style>
         </div>
     );
